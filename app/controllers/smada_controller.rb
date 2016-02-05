3b27346c4ba64efc0f8c19a4381fd325ff7dd4d9
Cattelan <Cattelan>
2016-02-05 16:01:02 -0200

    tentando iterar por todos os sensores

--- app/controllers/smada_controller.rb
+++ app/controllers/smada_controller.rb
@@ -17,8 +17,8 @@ class SmadaController < ApplicationController
 
     @dados = []
 
-    @sensorId['id_sensor'].each do |id|
-      @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 1 AND timestamp > '2016-01-30 11:00' AND id_sensor =" + id.to_s)
+    @sensorId.each do |id|
+      @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 1 AND timestamp > '2016-01-30 11:00' AND id_sensor =" + id['id_sensor'].to_s)
       @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 2 AND timestamp > '2016-01-30 11:00' AND id_sensor =" + id.to_s)
       @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 3 AND timestamp > '2016-01-30 11:00' AND id_sensor =" + id.to_s)
       @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 4 AND timestamp > '2016-01-30 11:00' AND id_sensor =" + id.to_s)
