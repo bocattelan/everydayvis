53937fe6f6e4df08863b0121728ce9dc1ada9bef
Cattelan <Cattelan>
2016-02-05 16:25:43 -0200

    agoravai

--- app/controllers/smada_controller.rb
+++ app/controllers/smada_controller.rb
@@ -19,12 +19,12 @@ class SmadaController < ApplicationController
 
     @sensorId.first.each do |id|
       @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 1 AND timestamp > '2016-01-30 11:00' AND id_sensor =" + id["id_sensor"].to_s)
-      @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 2 AND timestamp > '2016-01-30 11:00' AND id_sensor =" + id[:id_sensor].to_s)
-      @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 3 AND timestamp > '2016-01-30 11:00' AND id_sensor =" + id[:id_sensor].to_s)
-      @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 4 AND timestamp > '2016-01-30 11:00' AND id_sensor =" + id[:id_sensor].to_s)
-      @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 5 AND timestamp > '2016-01-30 11:00' AND id_sensor =" + id[:id_sensor].to_s)
-      @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 6 AND timestamp > '2016-01-30 11:00' AND id_sensor =" + id[:id_sensor].to_s)
-      @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 7 AND timestamp > '2016-01-30 11:00' AND id_sensor =" + id[:id_sensor].to_s)
+      @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 2 AND timestamp > '2016-01-30 11:00' AND id_sensor =" + id["id_sensor"].to_s)
+      @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 3 AND timestamp > '2016-01-30 11:00' AND id_sensor =" + id["id_sensor"].to_s)
+      @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 4 AND timestamp > '2016-01-30 11:00' AND id_sensor =" + id["id_sensor"].to_s)
+      @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 5 AND timestamp > '2016-01-30 11:00' AND id_sensor =" + id["id_sensor"].to_s)
+      @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 6 AND timestamp > '2016-01-30 11:00' AND id_sensor =" + id["id_sensor"].to_s)
+      @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 7 AND timestamp > '2016-01-30 11:00' AND id_sensor =" + id["id_sensor"].to_s)
     end
   end
 
