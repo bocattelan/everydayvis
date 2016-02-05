25da5f4251142f544bf4715b7394387479182a3e
Cattelan <Cattelan>
2016-02-05 13:40:51 -0200

    pegando as ultimas 6 horas - de forma feia ainda

--- app/controllers/smada_controller.rb
+++ app/controllers/smada_controller.rb
@@ -25,12 +25,12 @@ class SmadaController < ApplicationController
   def last_hours
     @dados = []
     @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 1 AND timestamp > '" + 6.hours.ago.to_s + "'")
-    @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 2 AND timestamp > '2016-01-30 11:00'")
-    @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 3 AND timestamp > '2016-01-30 11:00'")
-    @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 4 AND timestamp > '2016-01-30 11:00'")
-    @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 5 AND timestamp > '2016-01-30 11:00'")
-    @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 6 AND timestamp > '2016-01-30 11:00'")
-    @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 7 AND timestamp > '2016-01-30 11:00'")
+    @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 2 AND timestamp > '" + 6.hours.ago.to_s + "'")
+    @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 3 AND timestamp > '" + 6.hours.ago.to_s + "'")
+    @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 4 AND timestamp > '" + 6.hours.ago.to_s + "'")
+    @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 5 AND timestamp > '" + 6.hours.ago.to_s + "'")
+    @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 6 AND timestamp > '" + 6.hours.ago.to_s + "'")
+    @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 7 AND timestamp > '" + 6.hours.ago.to_s + "'")
   end
 
 end
