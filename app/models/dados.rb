65cbaa61df581c06b325d5a3d7504cdbc4cd17b9
Cattelan <Cattelan>
2016-02-05 11:28:45 -0200

    testes com o dados

--- app/models/dados.rb
+++ app/models/dados.rb
@@ -1,10 +1,9 @@
 class Dados < ActiveRecord::Base
-  self.table_name = "dados"
   def self.conn
     ActiveRecord::Base.establish_connection(
       :adapter  => "postgresql",
       :host     => "localhost",
-      :database => "sensordata",
+      :database => "dados",
       :user     => "postgres",
       :password => "city9"
     ).connection
