957e586d1bdc025bfe2b810c6a44af75829f2237
Cattelan <Cattelan>
2016-02-05 13:36:36 -0200

    tentando pegar os dados das ultimas 6 horas

--- app/controllers/smada_controller.rb
+++ app/controllers/smada_controller.rb
@@ -24,7 +24,7 @@ class SmadaController < ApplicationController
 
   def last_hours
     @dados = []
-    @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 1 AND timestamp > " + 6.hours.ago.to_s)
+    @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 1 AND timestamp > '" + 6.hours.ago.to_s + "'")
     @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 2 AND timestamp > '2016-01-30 11:00'")
     @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 3 AND timestamp > '2016-01-30 11:00'")
     @dados << Smada.conn.execute("SELECT * FROM dados WHERE sub_sensor_id = 4 AND timestamp > '2016-01-30 11:00'")
