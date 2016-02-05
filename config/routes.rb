541cd1798adb95e0fd1ea70b0c7fa2cb0458a2f7
Cattelan <Cattelan>
2016-02-05 13:31:42 -0200

    tentando pegar os dados das ultimas 6 horas

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
 
   get 'smada/dados', to: 'smada#dados'
   get 'smada/index', to: 'smada#index'
+  get 'smada/last_hours', to: 'smada#last_hours'
 
   get 'dataLogin' => 'login#index'
   get 'uploadFile' => 'import#index'
