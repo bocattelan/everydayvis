639486a1e9a2b9dd3d4c8d3ec19c89a47dc632e3
Cattelan <Cattelan>
2016-02-11 16:03:01 -0200

    botando titulos

--- app/controllers/smada_controller.rb
+++ app/controllers/smada_controller.rb
@@ -45,6 +45,9 @@ class SmadaController < ApplicationController
       @dados << Smada.conn.execute("SELECT * FROM data WHERE id_end_device = 6 AND timestamp > '" + 6.hours.ago.to_s + "' AND id_sensor =" + id["id_sensor"].to_s)
       @dados << Smada.conn.execute("SELECT * FROM data WHERE id_end_device = 7 AND timestamp > '" + 6.hours.ago.to_s + "' AND id_sensor =" + id["id_sensor"].to_s)
       @dados = @dados.compact
+      @dados.each do |query|
+        query.first = query.first.compact
+      end
     end
   end
   
