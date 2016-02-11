8ee6734b824aaec27f0a6631aa114b0072530fb3
Cattelan <Cattelan>
2016-02-11 15:25:17 -0200

    trocando nome de coluna do dat

--- app/models/dados.rb
+++ app/models/dados.rb
@@ -1,5 +1,5 @@
-class ::Data < ActiveRecord::Base
-
+class Dados < ActiveRecord::Base
+	self.table_name = "data"
   ActiveRecord::Base.establish_connection(
     :adapter  => "postgresql",
     :host     => "localhost",
