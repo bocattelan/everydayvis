96dddf2f91f378158bd14bb0089ed24f511e2898
bocattelan <bruno_cattelan02@hotmail.com>
2016-03-04 16:59:11 -0300

    teste com o db

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,5 +1,11 @@
 class Appointment < ActiveRecord::Base
-  ActiveRecord::Base.establish_connection DBCONF
+  #ActiveRecord::Base.establish_connection DBCONF
+    ActiveRecord::Base.establish_connection(
+    :adapter  => "postgresql",
+    :host     => "localhost",
+    :database => "everydayvis_development",
+    :encoding => "unicode"
+  ).connection
 
   belongs_to :person
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
-  ActiveRecord::Base.establish_connection DBCONF
+  #ActiveRecord::Base.establish_connection DBCONF
+    ActiveRecord::Base.establish_connection(
+    :adapter  => "postgresql",
+    :host     => "localhost",
+    :database => "everydayvis_development",
+    :encoding => "unicode"
+  ).connection
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable
