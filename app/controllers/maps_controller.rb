8df54cb8f7ebe309f559a53b8d42d745efefddb9
Cattelan <Cattelan>
2016-03-09 10:12:56 -0300

    dados de nome adicionados na mão, agora no maps também - MUDAR

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -14,6 +14,36 @@ class MapsController < ApplicationController
     @sensorName = []
     @sensorName = Sensor.all
 
+    @sensorName.each do |sensor|
+      if sensor["id_sensor"] == 1
+        sensor["sensor_name"] = 'Luminosidade'
+      elsif sensor["id_sensor"] == 2
+        sensor["sensor_name"] = 'temperatura'
+      elsif sensor["id_sensor"] == 3
+        sensor["sensor_name"] = 'Umidade'
+      elsif sensor["id_sensor"] == 4
+        sensor["sensor_name"] = 'Pressão Atmosférica'
+      elsif sensor["id_sensor"] == 5
+        sensor["sensor_name"] = 'Chuva'
+      elsif sensor["id_sensor"] == 6
+        sensor["sensor_name"] = 'Velocidade do vento'
+      elsif sensor["id_sensor"] == 7
+        sensor["sensor_name"] = 'Direção do vento'
+      elsif sensor["id_sensor"] == 10
+        sensor["sensor_name"] = 'Chuva (coordenador)'
+      elsif sensor["id_sensor"] == 11
+        sensor["sensor_name"] = 'CO'
+      elsif sensor["id_sensor"] == 12
+        sensor["sensor_name"] = 'CO2'
+      elsif sensor["id_sensor"] == 13
+        sensor["sensor_name"] = 'NO2'
+      elsif sensor["id_sensor"] == 14
+        sensor["sensor_name"] = 'UV'
+      elsif sensor["id_sensor"] == 15
+        sensor["sensor_name"] = 'DUST'
+      end
+    end
+
     @endDeviceId = []
     @endDeviceId = Dados.select('id_end_device').uniq
 
