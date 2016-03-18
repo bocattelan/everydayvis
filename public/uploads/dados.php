159db4e50953ec1cf77dd07cab74d5dbe3c62d73
bocattelan <bruno_cattelan02@hotmail.com>
2016-03-18 18:30:33 -0300

    testes php

--- public/uploads/dados.php
+++ public/uploads/dados.php
@@ -10,7 +10,7 @@
         //The actual data is found on this.responseText
         alert(this.responseText); //Will alert: 42
     };
-    oReq.open("get", "getAll.php", true);
+    oReq.open("get", "getALL.php", true);
     //                               ^ Don't block the rest of the execution.
     //                                 Don't wait until the request finishes to 
     //                                 continue.
