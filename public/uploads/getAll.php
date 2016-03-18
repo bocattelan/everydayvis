cdc3c55ed16a8bd13e5835275c8aa81fc0d5de0f
bocattelan <bruno_cattelan02@hotmail.com>
2016-03-18 18:22:54 -0300

    testes php

--- public/uploads/getAll.php
+++ public/uploads/getAll.php
@@ -8,12 +8,12 @@ $db = "sensordata";
 $con = pg_connect("host=$host dbname=$db user=$user port=$port")
     or die ("Could not connect to server\n"); 
 
-$query = "SELECT * FROM data"; 
+$query = "SELECT * FROM data LIMIT 1000"; 
 
 $rs = pg_query($con, $query) or die("Cannot execute query: $query\n");
 $rows = array();
 
-ini_set('memory_limit', '-1');
+//ini_set('memory_limit', '-1');
 
 while (array_push($rows,pg_fetch_assoc($rs))) {
     echo $rows[0]['id_sensor'] . " " . $rows[0]['id_end_device'] . " " . $rows[0]['payload'];
