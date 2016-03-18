145ef09b9f36106b7ba8ffd69203284a89fc684d
bocattelan <bruno_cattelan02@hotmail.com>
2016-03-18 17:59:49 -0300

    testes php

--- public/uploads/getAll.php
+++ public/uploads/getAll.php
@@ -12,9 +12,11 @@ $query = "SELECT * FROM sensors LIMIT 5";
 
 $rs = pg_query($con, $query) or die("Cannot execute query: $query\n");
 
-while ($row = pg_fetch_row($rs)) {
-  echo "$row[0] $row[1] $row[2] <br>";
+while ($row = pg_fetch_assoc($rs)) {
+    echo $row['id_sensor'] . " " . $row['payload'];
+    echo "<br>";
 }
+
 
 pg_close($con); 
 
