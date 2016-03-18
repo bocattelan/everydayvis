87af1c9adf023fc79c4fcdd66399d2287cb7fcda
bocattelan <bruno_cattelan02@hotmail.com>
2016-03-18 18:35:47 -0300

    testes php

--- public/uploads/getAll.php
+++ public/uploads/getAll.php
@@ -14,13 +14,13 @@ $rs = pg_query($con, $query) or die("Cannot execute query: $query\n");
 $rows = array();
 
 //ini_set('memory_limit', '-1');
-
+/*
 while (array_push($rows,pg_fetch_assoc($rs))) {
     echo $rows[0]['id_sensor'] . " " . $rows[0]['id_end_device'] . " " . $rows[0]['payload'];
     echo "<br>";
 }
-
-echo json_encode($rows);
+*/
+echo json_encode(array_values(pg_fetch_all($rs)));
 pg_close($con); 
 
 ?>
