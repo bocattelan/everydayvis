005bd7be63dae3f58b682ae4e2bf697e2ace0f83
Cattelan <Cattelan>
2016-03-18 11:26:17 -0300

    teste de php

---
+++ public/uploads/getAll.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once "conf.php";
+require_once "funcoes.php";
+
+
+//echo "<pre>";print_r($_GET);exit();
+
+//pg_retorna("SELECT * from data", $rQry);
+
+echo "iniciando...<br>";
+
+$sSql = "SELECT * FROM data"
+
+echo $sSql . "<br>";			   
+
+
+$executa = pg_executa($sSql);
+
+
+if ($executa) echo $executa;
+			   
+?>
