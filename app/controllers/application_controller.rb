164451d156550ffed2b93dfff9a629ae1da2e587
bruno cattelan <bocattelan@gmail.com>
2016-03-24 17:11:22 -0300

    FOI SIM testando cache

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,10 @@ class ApplicationController < ActionController::Base
   private
 
     def cache_other_db_connections
+      yield
       Smada.connection.cache 
       EveryDb.connection.cache 
-      yield
+
     end
 
   end
